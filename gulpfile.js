651fb50fdbdbceda124e05c4912a42285f089c46
Critonik <42637808+Critonik@users.noreply.github.com>
2019-06-05 21:03:01 +0500

    оптимизация

--- gulpfile.js
+++ gulpfile.js
@@ -50,7 +50,7 @@ gulp.task("clean", function () {
 });
 gulp.task("copy", function () {
   return gulp.src([
-    "source/fonts/**/*.otf",
+    "source/fonts/**/*.{woff,woff2}",
     "source/img/**",
     "source/js/**",
     "source/*.ico"
